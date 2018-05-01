3a2dc163305d0e1981edb3f08d1d83dc78a56b45
Joana Magalhães <jsousa039@gmail.com>
2018-05-01 11:24:49 +0200

    Changed file readingFiles.py
    Changed the original name of the file to ReadingFiles.py;
    Added new function connect_to_database where given connection details, it connects to that database;
    Added new function make_query which, given a SQL query, it returns the results of that query;
    Added new function query_to_json_file which, given a result from a query, a name of a column and a path to the result file, it turns the result of that query's column into a json file to the path given

---
+++ preprocessing/Joana/ReadingFiles.py
@@ -0,0 +1,33 @@
+import json
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+class ReadingFiles:
+
+    def __init__(self):
+        self.engine = None
+
+    def connect_to_database(self, uri):
+        # uri = 'mysql://root:123bloom@127.0.0.1/bloomdata'
+        self.engine = create_engine(uri)
+
+    def make_query(self, query):
+        # 'SELECT entry FROM requestlog LIMIT 10'
+        query = pd.read_sql(query, self.engine)
+        return query
+
+    @staticmethod
+    def query_to_json_file(query, column, path_file):
+        index = 0
+        data_json = []
+
+        while (index < len(query)):
+            j = query.get_value(index, column)
+            my_json = j.decode('utf8').replace("'", '"')
+            data = json.loads(my_json)
+            data_json.append(data)
+            index += 1
+
+        with open(path_file, 'w') as outfile:
+            json.dump(data_json, outfile)
