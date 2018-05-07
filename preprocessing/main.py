31804ad5d3d3168c08ffa89594a68fba3d53277b
Vlets <georgevle93@gmail.com>
2018-05-07 10:36:48 +0200

    Small changes

--- preprocessing/main.py
+++ preprocessing/main.py
@@ -1,13 +1,22 @@
 from preprocessing.JsonProcessor import JsonProcessor
+import numpy as np
+from kmodes.kprototypes import KPrototypes
 
 jsonTools = JsonProcessor()
 
 filePath = "/Users/george/Expendable/sample.json"
 
 sortBy = ["visitorId", "timestamp"]
+categoricalData = ["audience", "visitorId", "channel", "url", "geo", "newVisit"]
 
 data = jsonTools.json_read(filePath)
 sortedData = jsonTools.json_sort(data, sortBy)
 
 # Save to Json
-jsonTools.json_save(sortedData, "./testProduced.json")
+# jsonTools.json_save(sortedData, "./testProduced.json")
+
+#km = KPrototypes(n_clusters=4, init='Cao', verbose=16)
+
+#clusters = km.fit_predict(sortedData, categorical=categoricalData)
+
+#print(km.cluster_centroids_)
