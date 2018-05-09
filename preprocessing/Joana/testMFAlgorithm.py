c2c8c41e0583224855a2199caa98e68963b207b2
Vlets <georgevle93@gmail.com>
2018-05-09 10:34:09 +0200

    MockJson

--- preprocessing/Joana/testMFAlgorithm.py
+++ preprocessing/Joana/testMFAlgorithm.py
@@ -6,10 +6,23 @@ from preprocessing.JsonProcessor import JsonProcessor
 
 test = mfa.run_MF_algorithm(['A', 'B', 'C', 'D', 'C', 'B', 'E', 'G', 'H', 'G', 'W', 'A', 'O', 'U', 'O', 'V'])
 
+mockData = [{'visitorId': '1', 'pageUrl': 'mock.com/home', 'timestamp': '1'},
+            {'visitorId': '1', 'pageUrl': 'mock.com/home/afterhome', 'timestamp': '3'},
+            {'visitorId': '1', 'pageUrl': 'mock.com/home/afterhome/afterafterhome', 'timestamp': '5'},
+            {'visitorId': '1', 'pageUrl': 'mock.com/home', 'timestamp': '7'},
+            {'visitorId': '1', 'pageUrl': 'mock.com/home/thisshouldbealone', 'timestamp': '9'},
+            {'visitorId': '2', 'pageUrl': 'mock.com/home', 'timestamp': '1'},
+            {'visitorId': '2', 'pageUrl': 'mock.com/home/afterhome', 'timestamp': '3'},
+            {'visitorId': '3', 'pageUrl': 'mock.com/home/afterhome/afterafterhome', 'timestamp': '5'},
+            {'visitorId': '3', 'pageUrl': 'mock.com/home', 'timestamp': '7'},
+            {'visitorId': '3', 'pageUrl': 'mock.com/home/thisshouldbealone', 'timestamp': '9'}]
+
+dataFrame = pd.DataFrame(mockData)
+
 #Test the algorithm with actual data
 jsonTools = JsonProcessor()
 
-dataFrame = jsonTools.json_read("/Users/Joana/Documents/GitHub/scikitLiterallyLearn/preprocessing/Joana/test2.json")
+#dataFrame = jsonTools.json_read("/Users/Joana/Documents/GitHub/scikitLiterallyLearn/preprocessing/Joana/test2.json")
 
 sortBy = ["visitorId", "timestamp"]
 sortedData = jsonTools.json_sort(dataFrame, sortBy)
