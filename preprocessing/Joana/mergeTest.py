a643a65e5e2185718de09442760312df61e6b09b
Vlets <georgevle93@gmail.com>
2018-05-14 11:56:36 +0200

    mergefile to test

--- preprocessing/Joana/mergeTest.py
+++ preprocessing/Joana/mergeTest.py
@@ -8,19 +8,9 @@ dataFrameMock = pd.DataFrame(dataFiles.mockData)
 dataFrameMock2 = pd.DataFrame(dataFiles.mockData2)
 
 jsonTools = JsonProcessor()
-dataFrame = jsonTools.json_read("/Users/george/PycharmProjects/scikitLiterallyLearn/preprocessing/dataFiles/test2.json")
 
-sortBy = ["visitorId", "timestamp"]
-sortedData = jsonTools.json_sort(dataFrame, sortBy)
+sortedData = jsonTools.json_sort(dataFrameMock2, ['visitorId', 'timestamp'])
 
-start = time.time()
 mockResult = mfa.init_algorithm(sortedData)
-end = time.time()
-print(end - start)
 
-
-start = time.time()
-mockResult2 = mfa.init_algorithm(dataFrame)
-end = time.time()
-print(end - start)
-
+df = pd.DataFrame(mockResult, columns=['visitorId', 'transaction'])
