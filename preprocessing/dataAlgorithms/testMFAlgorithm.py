0ba6609b80daf220fee1ffd590337ccd02dfc779
Vlets <georgevle93@gmail.com>
2018-05-15 10:08:46 +0200

    readability and shortening

--- preprocessing/dataAlgorithms/testMFAlgorithm.py
+++ preprocessing/dataAlgorithms/testMFAlgorithm.py
@@ -6,21 +6,25 @@ import time
 
 dataFrameMock = pd.DataFrame(dataFiles.mockData)
 dataFrameMock2 = pd.DataFrame(dataFiles.mockData2)
+path = "/Users/george/PycharmProjects/scikitLiterallyLearn/preprocessing/dataFiles/test2.json"
 
 jsonTools = JsonProcessor()
-dataFrame = jsonTools.json_read("/Users/george/PycharmProjects/scikitLiterallyLearn/preprocessing/dataFiles/test2.json")
 
-sortBy = ["visitorId", "timestamp"]
-sortedData = jsonTools.json_sort(dataFrame, sortBy)
+# unsorted data
+dataFrame = jsonTools.json_read(path)
+dataFrame = jsonTools.normalize_collectors(dataFrame)
+
+# normal data
+sortedData = jsonTools.do_it_all(path)
 
 start = time.time()
 mockResult = mfa.init_algorithm(sortedData)
 end = time.time()
-print(end - start)
+print(end - start, "- Sorted Data")
 
 
 start = time.time()
 mockResult2 = mfa.init_algorithm(dataFrame)
 end = time.time()
-print(end - start)
+print(end - start, "- Unsorted data")
 
