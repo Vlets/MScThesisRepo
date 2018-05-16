2f7228bc54ce5306e1597b4fe6c777e4c86ab354
Vlets <georgevle93@gmail.com>
2018-05-16 10:30:01 +0200

    Changed UnitTests

--- preprocessing/unitTests/preProcessingUnitTests.py
+++ preprocessing/unitTests/preProcessingUnitTests.py
@@ -14,66 +14,66 @@ class UnitTests(unittest.TestCase):
     goodReorderedData = dataFiles.mockDataReordered
 
     def test_MFA_good_data(self):
-        processedData = self.process_data(self.goodData)
-        result = mfa.init_algorithm(processedData)
-        expectedResult = [('1aUserOne',
-                           [(1,
-                             ['mock.com/home',
-                              'mock.com/home/afterhome',
-                              'mock.com/home/afterhome/afterafterhome']),
-                            (1000, ['mock.com/home', 'mock.com/home/thisshouldbealone'])]),
-                          ('2bUserTwo', [(32, ['mock.com/home', 'mock.com/home/afterhome'])]),
+        processed_data = self.process_data(self.goodData)
+        result = mfa.init_algorithm(processed_data)
+        expected_result = [('1aUserOne',
+                           1,
+                           ['mock.com/home',
+                            'mock.com/home/afterhome',
+                            'mock.com/home/afterhome/afterafterhome']),
+                          ('1aUserOne', 1000, ['mock.com/home', 'mock.com/home/thisshouldbealone']),
+                          ('2bUserTwo', 32, ['mock.com/home', 'mock.com/home/afterhome']),
                           ('3cUserThree',
-                           [(8932,
-                             ['mock.com/home/afterhome/afterafterhome',
-                              'mock.com/home',
-                              'mock.com/home/thisshouldbealone'])])]
-        self.assertEqual(result, expectedResult)
+                           8932,
+                           ['mock.com/home/afterhome/afterafterhome',
+                            'mock.com/home',
+                            'mock.com/home/thisshouldbealone'])]
+        self.assertEqual(result, expected_result)
 
     def test_MFA_random_timestamp_data(self):
-        processedData = self.process_data(self.badData)
-        result = mfa.init_algorithm(processedData)
-        expectedResult = [('Annie',
-                           [(1, ['mock.com/home', 'mock.com/home/afterhome']),
-                            (1000,
-                             ['mock.com/home',
-                              'mock.com/home/thispageisdifferent',
-                              'mock.com/home/afterhome/afterafterhome'])]),
+        processed_data = self.process_data(self.badData)
+        result = mfa.init_algorithm(processed_data)
+        expected_result = [('Annie', 1, ['mock.com/home', 'mock.com/home/afterhome']),
+                          ('Annie',
+                           1000,
+                           ['mock.com/home',
+                            'mock.com/home/thispageisdifferent',
+                            'mock.com/home/afterhome/afterafterhome']),
                           ('Smooth Criminal',
-                           [(89323,
-                             ['mock.com/home/afterhome/afterafterhome',
-                              'mock.com/home',
-                              'mock.com/home/thispageisdifferent/iswear'])]),
-                          ("You've been hit", [(34, ['mock.com/home', 'mock.com/home/afterhome'])]),
-                          ('are_you', [(32, ['mock.com/home', 'mock.com/home/afterhome'])]),
-                          ('by a', [(10001, ['mock.com/home', 'mock.com/home/thispageisdifferent'])]),
+                           89323,
+                           ['mock.com/home/afterhome/afterafterhome',
+                            'mock.com/home',
+                            'mock.com/home/thispageisdifferent/iswear']),
+                          ("You've been hit", 34, ['mock.com/home', 'mock.com/home/afterhome']),
+                          ('are_you', 32, ['mock.com/home', 'mock.com/home/afterhome']),
+                          ('by a', 10001, ['mock.com/home', 'mock.com/home/thispageisdifferent']),
                           ('ok?',
-                           [(8932,
-                             ['mock.com/home/afterhome/afterafterhome',
-                              'mock.com/home',
-                              'mock.com/home/thisshouldbealone'])])]
-        self.assertEqual(result, expectedResult)
+                           8932,
+                           ['mock.com/home/afterhome/afterafterhome',
+                            'mock.com/home',
+                            'mock.com/home/thisshouldbealone'])]
+        self.assertEqual(result, expected_result)
 
     # reset index makes sure the index numbers do not interfere in the assertion.
     def test_sorting(self):
         result1 = (self.process_data(self.goodData)).reset_index(drop=True)
         result2 = (self.process_data(self.goodReorderedData)).reset_index(drop=True)
-        compareDataframes = result1.equals(result2)
-        self.assertTrue(compareDataframes)
+        compare_data_frames = result1.equals(result2)
+        self.assertTrue(compare_data_frames)
 
     # Checks if the length of the resulting list is the same as the number
-    # of unique visitorIds in the initial dataFrame.
+    # of unique visitorIds in the initial dataframe.
     def test_did_we_miss_visitors(self):
-        sortedData = self.jsonTools.read_and_sort_data(
-            "/Users/george/PycharmProjects/scikitLiterallyLearn/preprocessing/dataFiles/test2.json")
-        mockResult = mfa.init_algorithm(sortedData)
-        self.assertTrue(len(mockResult) == sortedData['visitorId'].nunique())
+        path = "/Users/george/PycharmProjects/scikitLiterallyLearn/preprocessing/dataFiles/test2.json"
+        sorted_data = self.jsonTools.read_and_sort_data(path)
+        mock_result = self.jsonTools.do_it_all(path)
+        self.assertTrue(mock_result['visitorId'].nunique() == sorted_data['visitorId'].nunique())
 
     def process_data(self, data):
-        dataFrame = pd.DataFrame(data)
-        sortBy = ["visitorId", "timestamp"]
-        sortedData = self.jsonTools.json_sort(dataFrame, sortBy)
-        return sortedData
+        data_frame = pd.DataFrame(data)
+        sort_by = ["visitorId", "timestamp"]
+        sorted_data = self.jsonTools.json_sort(data_frame, sort_by)
+        return sorted_data
 
 
 if __name__ == '__main__':
