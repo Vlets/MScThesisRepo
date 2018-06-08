d9034c9041a62ce063e79ffd8c610b568c5c196c
Vlets <georgevle93@gmail.com>
2018-06-08 16:50:04 +0200

    Notes

--- segmentation/main.py
+++ segmentation/main.py
@@ -5,7 +5,6 @@ from sklearn.metrics import silhouette_score
 from segmentation.helpers.JsonProcessor import JsonProcessor
 from segmentation.helpers import urlKeywordExtractor as urlExtract
 
-
 jsonTools = JsonProcessor()
 
 filePath = "/Users/george/PycharmProjects/scikitLiterallyLearn/segmentation/dataFiles/test2.json"
@@ -14,17 +13,21 @@ processedData = '/Users/george/PycharmProjects/scikitLiterallyLearn/segmentation
 
 # THE ONE FUNCTION TO RULE THEM ALL IS pipeline(filePath)
 
-data_frame = jsonTools.pre_process(bigData)
+data_frame = jsonTools.pre_process(filePath)
 
 data_frame = data_frame.drop(['geo.country', 'transactionPath'], axis=1)
 data_frame = data_frame.astype(str)
 
-kmodes_cao = KModes(n_clusters=12, init='Cao', verbose=1)
+kmodes_cao = KModes(n_clusters=10, init='Cao', verbose=1)
 kmodes_cao.fit_predict(data_frame)
 
 column_names = list(data_frame.columns.values)
 clusters = pd.DataFrame(kmodes_cao.cluster_centroids_, columns=column_names)
 
+a = pd.read_csv(
+    "/Users/george/PycharmProjects/scikitLiterallyLearn/segmentation/dataFiles/8juneSmallPCM.csv")
+b = kmodes_cao.labels_
+print("Silhouette score:", silhouette_score(a, b, metric='precomputed'))
 
 # Save to Json/CSV. If you don't want to specify a path, simply put the filename.
-# jsonTools.json_save(sortedData, "./dataFiles/testProduced", toJson=False)
+# jsonTools.json_save(sortedData, "./dataFiles/testProduced", to_json=False)
