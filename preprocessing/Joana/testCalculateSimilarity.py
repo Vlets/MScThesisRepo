aba382be18c732a357880c62f572abbca88030f0
Joana Magalhães <jsousa039@gmail.com>
2018-06-19 15:36:02 +0200

    Changed the file testCalculateSimilarity
    - Added some lines of code to make more tests on the file testCalculateSimilarity
    - Removed file CalculateSimilarity from folder Joana and put it on folder dataAlgorithms

--- preprocessing/Joana/testCalculateSimilarity.py
+++ preprocessing/Joana/testCalculateSimilarity.py
@@ -1,4 +1,8 @@
+from preprocessing.dataAlgorithms.CalculateSimilarity import CalculateSimilarity as cs
+
 str1 = "about architecture deployment about deployment about releasenotes about architecture deployment"
 str2 = "about development cms development upgrade upgrade upgrade installation upgrade"
 str3 = "how-do-i installation tutorial best-practices configuration development how-do-i relevance tutorial development tutorial tutorial"
 str4 = "configuration how-do-i integration security configuration how-do-i integration security configuration development how-do-i"
+
+similarities = cs.similarity_results([str1], [str2, str3, str4])
--- preprocessing/dataAlgorithms/CalculateSimilarity.py
+++ preprocessing/dataAlgorithms/CalculateSimilarity.py
@@ -14,15 +14,19 @@ class CalculateSimilarity:
         vector = TfidVectorizer(tokenizer=CalculateSimilarity.tokens)
         tfidf = vector.fit_transform(used_items+not_used_items)
         used_items_vectors = [x.reshape(1, -1) for x in tfidf.toarray()[:used_items_length]]
+        used_items_vectors = list(zip(used_items_vectors, used_items))
         not_used_items_vectors = [x.reshape(1, -1) for x in tfidf.toarray()[used_items_length:]]
+        not_used_items_vectors = list(zip(not_used_items_vectors, not_used_items))
         paired_items = []
         similarities_result = []
 
         for r in itertools.product(used_items_vectors, not_used_items_vectors):
             paired_items.extend([(r[0], r[1])])
 
-        for item1, item2 in paired_items:
+        for key, value in paired_items:
+            item1, name1 = key
+            item2, name2 = value
             similarity = cosine_similarity(item1, item2)
-            similarities_result.extend([(similarity.item(0, 0), item1, item2)])
+            similarities_result.extend([(similarity.item(0, 0), name1, name2)])
 
-        return similarities_result.sort()
+        return similarities_result
