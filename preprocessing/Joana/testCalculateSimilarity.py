5b34b9a74a90591c85bb5095ecf19d32b2b7ed0a
Joana Magalhães <jsousa039@gmail.com>
2018-06-19 16:23:40 +0200

    Added some other exaples to test on file testCalculateSimilarity

--- preprocessing/Joana/testCalculateSimilarity.py
+++ preprocessing/Joana/testCalculateSimilarity.py
@@ -5,4 +5,17 @@ str2 = "about development cms development upgrade upgrade upgrade installation u
 str3 = "how-do-i installation tutorial best-practices configuration development how-do-i relevance tutorial development tutorial tutorial"
 str4 = "configuration how-do-i integration security configuration how-do-i integration security configuration development how-do-i"
 
-similarities = cs.similarity_results([str1], [str2, str3, str4])
+web_page_categories1 = "deployment development installation respository"
+web_page_categories2 = "cms components configuration deployment"
+web_page_categories3 = "components development"
+web_page_categories4 = "development deployment components"
+web_page_categories5 = "configuration development how-do-i"
+web_page_categories6 = "development how-do-i relevance"
+web_page_categories7 = "architecture development hst about"
+
+similarities_str = cs.similarity_results([str1], [str2, str3, str4])
+similarities_web = cs.similarity_results([web_page_categories1, web_page_categories6], [web_page_categories2,
+                                                                                        web_page_categories3,
+                                                                                        web_page_categories4,
+                                                                                        web_page_categories5,
+                                                                                        web_page_categories7])
