93f83df591d893037ae9e5df317f1601ab41867f
Joana Magalhães <jsousa039@gmail.com>
2018-06-24 18:05:36 +0200

    Deep Neural Network trainig working

--- preprocessing/Joana/MainDNN.py
+++ preprocessing/Joana/MainDNN.py
@@ -1,14 +1,11 @@
-from preprocessing.helpers.JsonProcessor import JsonProcessor
-from preprocessing.dataAlgorithms.NormalizePersona import NormalizePersona
-from preprocessing.dataAlgorithms.ReadingFiles import ReadingFiles as rf
-from sklearn.preprocessing import LabelEncoder
+from preprocessing.Joana.PreprocessingData import PreprocessingData
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 from keras.models import Sequential
 from keras.layers import Dense
+import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
-from preprocessing.Joana.PreprocessingData import PreprocessingData
-import math
 # Importing the Keras libraries and packages
 
 
@@ -20,9 +17,10 @@ list_categories = PreprocessingData.create_categories_list(sortedData, 'categori
 sortedData = sortedData.reset_index(drop=True)
 
 categories_table = sortedData[list_categories]
+sortedData['null'] = sortedData.null.apply(lambda x: x if pd.notna(x) else 0.0)
 
 #1st step, turn columns into strings
-#sortedData['geo_city'] = sortedData['geo_city'].astype(str)
+sortedData['geo_city'] = sortedData['geo_city'].astype(str)
 sortedData['geo_continent'] = sortedData['geo_continent'].astype(str)
 sortedData['geo_country'] = sortedData['geo_country'].astype(str)
 
@@ -33,19 +31,20 @@ Y = categories_table.iloc[:, :].values
 
 #3rd step, drop the labels
 list_to_drop = list_categories
-list_to_drop.extend(['categories', 'geo_city'])
+list_to_drop.extend(['categories'])
 sortedData = sortedData.drop(columns=list_to_drop)
 
 #4th step, use one hot enconding on the table
-#result_cities = pd.get_dummies(sortedData['geo_city'])
+result_cities = pd.get_dummies(sortedData['geo_city'])
 result_continent = pd.get_dummies(sortedData['geo_continent'])
 result_country = pd.get_dummies(sortedData['geo_country'])
 result_persona_id = pd.get_dummies(sortedData['personaIdScores_id'])
 result_global_persona_id = pd.get_dummies(sortedData['globalPersonaIdScores_id'])
 #sortedData = sortedData.drop(columns=['geo_city', 'geo_continent', 'geo_country', 'personaIdScores_id',
 #                                      'globalPersonaIdScores_id'])
-testing_table = sortedData.drop(columns=['geo_continent', 'geo_country', 'personaIdScores_id', 'globalPersonaIdScores_id'])
-testing_table = pd.concat([testing_table, result_continent, result_country, result_persona_id,
+testing_table = sortedData.drop(columns=['geo_city', 'geo_continent', 'geo_country', 'personaIdScores_id',
+                                         'globalPersonaIdScores_id'])
+testing_table = pd.concat([testing_table, result_cities, result_continent, result_country, result_persona_id,
                         result_global_persona_id], axis=1)
 
 #5th step, get the wanted input
@@ -60,12 +59,16 @@ X = testing_table.iloc[:, :].values
 #7th step, encode the label
 
 #8th step. split the dataset into the training set and test set
-#X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2)
 
 length_x = len(testing_table.columns.values.tolist())
 length_y = len(categories_table.columns.values.tolist())
 
-X_train = X[:8982]
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+"""X_train = X[:8982]
 X_test = X[8982:]
 
 Y_train = Y[:8982]
@@ -75,7 +78,7 @@ X_val = X_train[:1000]
 partial_x_train = X_train[1000:]
 
 Y_val = Y_train[:1000]
-partial_y_train = Y_train[1000:]
+partial_y_train = Y_train[1000:]"""
 
 
 
@@ -83,15 +86,40 @@ partial_y_train = Y_train[1000:]
 model = Sequential()
 model.add(Dense(64, activation='relu', input_shape=(length_x,)))
 model.add(Dense(64, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(64, activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(16, activation='relu'))
 model.add(Dense(length_y, activation='sigmoid'))
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 #10th step, training the model
-#history = model.fit(X_train,
-#                    Y_train,
-#                    epochs=20,
-#                    batch_size=100,
-#                    validation_data = (X_test, Y_test))
-#
+history = model.fit(X_train,
+                    Y_train,
+                    epochs=20,
+                    batch_size=512,
+                    validation_data=(X_test, Y_test))
+
+results = model.evaluate(X_test, Y_test)
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+plt.clf()
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
--- preprocessing/dataAlgorithms/NormalizePersona.py
+++ preprocessing/dataAlgorithms/NormalizePersona.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from pandas.io.json import json_normalize
 
 
@@ -10,18 +9,20 @@ class NormalizePersona:
 
         globalPersonaIdScores = [pd.DataFrame(json_normalize(x)) for x in [[y[0]] if len(y) > 1 else y for y
                                                                            in sortedData['globalPersonaIdScores']]]
-        globalPersonaIdScores = [pd.DataFrame(data={'id': ["None"], 'score': [np.nan]}) if elem.empty else elem for elem
+        globalPersonaIdScores = [pd.DataFrame(data={'id': ["None"], 'score': [0.0]}) if elem.empty else elem for elem
                                  in globalPersonaIdScores]
         globalPersonaIdScores = pd.concat(globalPersonaIdScores)
         globalPersonaIdScores.columns = ['globalPersonaIdScores_id', 'globalPersonaIdScores_score']
         globalPersonaIdScores = globalPersonaIdScores.reset_index(drop=True)
+        globalPersonaIdScores['globalPersonaIdScores_score'] = globalPersonaIdScores['globalPersonaIdScores_score'].div(100)
         personaIdScores = [pd.DataFrame(json_normalize(x)) for x in [[y[0]] if len(y) > 1 else y for y
                                                                      in sortedData['personaIdScores']]]
-        personaIdScores = [pd.DataFrame(data={'id': ["None"], 'score': [np.nan]}) if elem.empty else elem for elem
+        personaIdScores = [pd.DataFrame(data={'id': ["None"], 'score': [0.0]}) if elem.empty else elem for elem
                            in personaIdScores]
         personaIdScores = pd.concat(personaIdScores)
         personaIdScores.columns = ['personaIdScores_id', 'personaIdScores_score']
         personaIdScores = personaIdScores.reset_index(drop=True)
+        personaIdScores['personaIdScores_score'] = personaIdScores['personaIdScores_score'].div(100)
         sortedData = sortedData.drop(columns=['globalPersonaIdScores', 'personaIdScores'])
 
         return pd.concat([sortedData, globalPersonaIdScores, personaIdScores], axis=1)
