669b2c4429202fba70cbe6b1554a80c6aeecd0ad
Joana Magalhães <jsousa039@gmail.com>
2018-06-25 17:11:11 +0200

    Changes done on Main_Part1

--- preprocessing/Joana/Main_Part1.py
+++ preprocessing/Joana/Main_Part1.py
@@ -2,7 +2,7 @@ from preprocessing.helpers.ReadingFiles import ReadingFiles as rf
 from preprocessing.helpers.PreprocessingData import PreprocessingData
 from preprocessing.Joana.DNNModel import DNNModel
 
-uri = 'mysql://root:123bloom@127.0.0.1/bloomreachdatabase'
+"""uri = 'mysql://root:123bloom@127.0.0.1/bloomreachdatabase'
 
 reading_files = rf()
 reading_files.connect_to_database(uri)
@@ -14,11 +14,16 @@ reading_files.query_to_json_file(query_result, 'entry',
                                  "/Users/Joana/Documents/GitHub/scikitLiterallyLearn/preprocessing/Joana"
                                  "/bloomreach_targeting.json")
 
-
+"""
 pre_data = PreprocessingData()
 dnn_model = DNNModel()
 
 initial_table = pre_data.run_preprocessing("./bloomreach_targeting.json")
+visitor = '3efb952b-21ef-4ebf-b307-cb32ac1eba51'
+
+visitor_indexes = initial_table.index[initial_table['visitorId'] == visitor].tolist()
+#initial_table = initial_table[initial_table['visitorId'] != visitor]
+#initial_table = initial_table.reset_index(drop=True)
 
 pre_data.preprocessing_DNN(initial_table, "./processed_bloomreach_targeting.json")
 
@@ -33,4 +38,3 @@ dnn_model.create_model(length_x, length_y)
 
 results, predictions, accuracy = dnn_model.train_model(X, Y)
 
-visitor = '3efb952b-21ef-4ebf-b307-cb32ac1eba51'
